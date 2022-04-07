37a5e36db43b8df72ae41a11434bea187af43ba5
Chris <github.account@chrigel.net>
2022-04-07 11:29:13 +0200

    Update vshn/antora images to v3.0.1

--- docs/antora-preview.mk
+++ docs/antora-preview.mk
@@ -1,5 +1,5 @@
 
-antora_preview_version ?= 2.3.14
+antora_preview_version ?= 3.0.1.1
 antora_preview_cmd ?= $(DOCKER_CMD) run --rm --publish 2020:2020 --volume "${PWD}":/preview/antora docker.io/vshn/antora-preview:$(antora_preview_version) --style=vshn
 
 .PHONY: docs-preview
