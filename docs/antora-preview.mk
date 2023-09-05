3bbe16177d7a40c91ee84042b2f100d05fcffb81
Simon Beck <simon.beck@vshn.ch>
2023-09-05 08:56:07 +0200

    Add some docs

--- docs/antora-preview.mk
+++ docs/antora-preview.mk
@@ -1,6 +1,6 @@
 
-antora_preview_version ?= 3.0.1.1
-antora_preview_cmd ?= $(DOCKER_CMD) run --rm --publish 2020:2020 --volume "${PWD}":/preview/antora docker.io/vshn/antora-preview:$(antora_preview_version) --style=vshn
+antora_preview_version ?= 3.1.4
+antora_preview_cmd ?= $(DOCKER_CMD) run --rm --publish 35729:35729 --publish 2020:2020 --volume "${PWD}":/preview/antora ghcr.io/vshn/antora-preview:$(antora_preview_version) --style=vshn
 
 .PHONY: docs-preview
 docs-preview: ## Preview the documentation
